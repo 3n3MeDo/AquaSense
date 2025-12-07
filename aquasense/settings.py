61e34c04a6c9c9f558f955ea57aa4ab7bfa62c74
muhammedelshazly <muhammedelshazly.eng@gmail.com>
2025-12-07 21:46:46 +0200

    Configure PostgreSQL support for Railway

--- aquasense/settings.py
+++ aquasense/settings.py
@@ -77,11 +77,13 @@ WSGI_APPLICATION = 'aquasense.wsgi.application'
 #              DATABASE
 # ==========================================
 
+import dj_database_url
+
 DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
-    }
+    'default': dj_database_url.config(
+        default='sqlite:///' + str(BASE_DIR / 'db.sqlite3'),
+        conn_max_age=600
+    )
 }
 
 # ==========================================
